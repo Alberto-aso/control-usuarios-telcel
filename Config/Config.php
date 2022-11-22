32fd2900adeb4fcf4ff28c1c42670354cddee3a5
alberto.asow@gmail.com <alberto.asow@gmail.com>
2022-11-22 12:47:48 -0700

    Add base_url

--- Config/Config.php
+++ Config/Config.php
@@ -1,6 +1,7 @@
 <?php
 //Const para la base URL de la aplicaion¡
-const base_url = "http://localhost/PHP_projects/oym_proyectos_new_template/control-usuarios/user_control/";
+//const base_url = "http://localhost/PHP_projects/oym_proyectos_new_template/control-usuarios/user_control/";
+const base_url = "https://control-usuarios-telcel-production.up.railway.app/";
 
 //Constantes para la conexion a Servidor 100 -> control veiculas tabla usuarios
 const host = "localhost";
