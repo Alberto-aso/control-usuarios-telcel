44bebc7d15f34661cfd55b147901d8a81a35ca93
alberto.asow@gmail.com <alberto.asow@gmail.com>
2022-11-22 13:24:05 -0700

    cambio en htaccess

--- Config/Config.php
+++ Config/Config.php
@@ -1,12 +1,17 @@
 <?php
 //Const para la base URL de la aplicaion¡
-//const base_url = "http://localhost/PHP_projects/oym_proyectos_new_template/control-usuarios/user_control/";
-const base_url = "https://control-usuarios-telcel-production.up.railway.app/";
+//const base_url = "http://localhost/PHP_projects/oym_proyectos_new_template/control-usuarios/user_control/"; //Variable para aplicacion loca
+const base_url = "https://control-usuarios-telcel-production.up.railway.app/"; //Variable para servidor Railway
 
 //Constantes para la conexion a Servidor 100 -> control veiculas tabla usuarios
-const host = "localhost";
-const user = "root";
-const password = "";
-const db = "control_usuarios";
+const host = "localhost"; //Variable localhost para mysql
+const user = "root"; //Variable local user para mysql
+const password = ""; //variable password local para mysql
+const db = "control_usuarios"; //Variable base de datos para local mysql
 
-
+/*
+const host = "containers-us-west-134.railway.app"; //Variable servidor para mysql
+const user = "root"; //Variable servidor user para mysql
+const password = "PJ4UPwkJHWtqOFtyxpvv"; //variable servidor local para mysql
+const db = "railway"; //Variable base de datos para servidor mysql
+*/
